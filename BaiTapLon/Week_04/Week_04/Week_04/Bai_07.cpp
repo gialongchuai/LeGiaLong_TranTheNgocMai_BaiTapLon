8400ed5f6b7597f725ff68c9d5742bd4aa7beaa0
gialongchuai <minhtan0949@gmail.com>
2024-08-05 17:10:50 +0700

    LeGiaLong: finish Tuan_04 Bai-07: Kiểm tra xem chuỗi nhập vào có đối xứng hay không?

---
+++ BaiTapLon/Week_04/Week_04/Week_04/Bai_07.cpp
@@ -0,0 +1,38 @@
+﻿/*
+	Bài 7. Kiểm tra xem chuỗi nhập vào có đối xứng hay không?
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Hàm kiểm tra chuỗi đối xứng
+bool kiemTraDoiXung(char str[]) {
+	int length = strlen(str);
+	for (int i = 0; i < length / 2; i++) {
+		if (str[i] != str[length - 1 - i]) {
+			return false; // Chuỗi không đối xứng
+		}
+	}
+	return true; // Chuỗi đối xứng
+}
+
+int main() {
+	char str[100];
+
+	// Nhập chuỗi từ người dùng
+	printf("Nhap vao chuoi can kiem tra: ");
+	gets(str);
+
+	// Kiểm tra chuỗi đối xứng và in kết quả
+	if (kiemTraDoiXung(str)) {
+		printf("Chuoi '%s' la chuoi doi xung.\n", str);
+	}
+	else {
+		printf("Chuoi '%s' khong phai la chuoi doi xung.\n", str);
+	}
+
+	system("pause");
+	return 0;
+}
